44a94dce9fdc939ced2781a2bd7a01b33c9a4ec1
Pratik <danpheaus@gmail.com>
2022-02-07 15:56:16 +1000

    new pages added

---
+++ includes/sidebar 2.php
@@ -0,0 +1,76 @@
+
+
+<!--Blog Sidebar Widgets Column -->
+
+  <div class="col-md-4">
+      
+    
+                <!-- Blog Search Well -->
+                <div class="well">
+                    <h4>Blog Search</h4>
+                    <form action="search.php" method="post">
+                    <div class="input-group">
+                        <input name="search" type="text" class="form-control">
+                        <span class="input-group-btn">
+                            <button name="submit" class="btn btn-default" type="submit">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                        </span>
+                    </div>
+                    </form><!--search form -->
+                    <!-- /.input-group -->
+                </div>
+
+
+
+
+
+
+
+                <!-- Blog Categories Well -->
+                    <div class="well">
+
+                        <?php
+                        //  $connection = mysqli_connect("localhost","root","root","cms"); if db is not included
+                            $query = "SELECT * FROM categories LIMIT 3";
+                            $select_categories_sidebar = mysqli_query($connection,$query);
+                        ?>
+
+                        <h4>Blog Categories</h4>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <ul class="list-unstyled">
+                                <?php
+                                    while($row = mysqli_fetch_array($select_categories_sidebar)){
+                                    $cat_title = $row['cat_title'];
+                                    echo "<li><a href='#'>{$cat_title}<a></li>";
+                                    }
+                                ?>
+                                </ul>
+                            </div>
+
+                                <!-- /.col-lg-6 -->
+                                <!--
+                                <div class="col-lg-6">
+                                    <ul class="list-unstyled">
+                                        <li><a href="#">Category Name</a>
+                                        </li>
+                                        <li><a href="#">Category Name</a>
+                                        </li>
+                                        <li><a href="#">Category Name</a>
+                                        </li>
+                                        <li><a href="#">Category Name</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                    -->
+                                <!-- /.col-lg-6 -->
+
+                        </div>
+                    <!-- /.row -->
+                    </div>
+
+                <!-- Side Widget Well -->
+              <?php include "includes/widget.php"; ?>
+
+</div>
---
+++ includes/widget 2.php
@@ -0,0 +1,4 @@
+<div class="well">
+    <h4>Side Widget Well</h4>
+    <p>Be a blogger. Post your thoughts and enable change.</p>
+</div>
