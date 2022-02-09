8c91dd10762dd4bdc5ef096213e698692267d9e3
Pratik Khadka <16739871+realpratik@users.noreply.github.com>
2022-02-09 11:05:43 +1000

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,15 +1,11 @@
 <?php
 
-$db['db_host'] = 'localhost';
-$db['db_user'] = 'root';
-$db['db_pass'] = 'root';
-$db['db_name'] = 'cms';
+$db['db_host'] = '';
+$db['db_user'] = '';
+$db['db_pass'] = '';
+$db['db_name'] = '';
 
 
-// $db['db_host'] = '127.0.0.1';
-// $db['db_user'] = 'u4gxfjp6klfxf';
-// $db['db_pass'] = 'w38emok43gvv';
-// $db['db_name'] = 'dbgnnigahhwhgm';
 
 foreach($db as $key => $value){
   define(strtoupper($key), $value);
